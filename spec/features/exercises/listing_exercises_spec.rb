4e839902bf1fbb91335bba3c7af525a33eade12e
Christopher Camenares <camenarescoding@gmail.com>
2017-05-31 14:25:55 -0400

    RSpec for no workouts yet

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -31,4 +31,22 @@ RSpec.feature "Listing Exercises" do
 		#expect(page).not_to have_content(@e3.workout_date)
 	end
 
+	scenario "shows no exercises if none created" do 
+		@john.exercises.delete_all
+
+		visit "/"
+
+		click_link "My Lounge"
+
+		expect(page).to have_content("No Workouts Yet")
+
+		expect(page).not_to have_content(@e1.duration_in_min)
+		expect(page).not_to have_content(@e1.workout)
+		expect(page).not_to have_content(@e1.workout_date)
+
+		expect(page).not_to have_content(@e2.duration_in_min)
+		expect(page).not_to have_content(@e2.workout)
+		expect(page).not_to have_content(@e2.workout_date)
+	end
+
 end
