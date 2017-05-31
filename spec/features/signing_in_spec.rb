4fa1aad3229c100a1fe7d62b8d4a3f5263b32c95
Christopher Camenares <camenarescoding@gmail.com>
2017-05-31 10:31:52 -0400

    Implement User Sign Up

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -1,21 +1,18 @@
 require 'rails_helper'
 
-RSpec.feature "A user signs in" do 
-	before do 
-		@john = User.create(email: "john@example.com", password: "password")
-	end
-
-	scenario do 
+RSpec.feature "Users signup" do 
+	scenario "wth valid credentials" do 
 		visit "/"
-		click_link "Sign In"
-		fill_in "Username", with: "john@example.com"
-		fill_in "Password", with: "password"
-		click_button "Sign In"
 
-		expect(page).to have_content("You have succesfully signed in.")
-		expect(page).to have_content("Signed in as #{@john.email}")
-		expect(page).not_to have_link("Sign in")
-		expect(page).not_to have_link("Sign up")
+		click_link "Sign up"
+
+		fill_in "Email", with: "john@example.com"
+		fill_in "Password", with: "password"
+		fill_in "Password confirmation", with: "password"
+
+		click_button "Sign up"
+
+		expect(page).to have_content("You have signed up successfully.")
 
 	end
 
